fb3e52b3c0b97976c494ebb71a8786c7aeb10616
Bulat Abilmansur <b.mansur2111@gmail.com>
2026-03-28 18:07:52 +0500

    Final fix for 404

---
+++ static/js/488.0ad15f4c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkdecentrathon_5_0=globalThis.webpackChunkdecentrathon_5_0||[]).push([[488],{488(e,a,n){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.0ad15f4c.chunk.js.map
